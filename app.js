4a6c0b627d04b3b5a865565f0d7cd5967ddd7ef6
Charly <123911937+CharlyKrDev@users.noreply.github.com>
2023-11-02 11:19:49 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-let nombre = prompt("Hola! ingresa tu nombre, por favor!");
-let apellido = prompt("Ahora tu apellido.");
-let edad = parseInt(prompt("Cual es tu edad? Gracias"));
-let nombreChequeado = true;
-let apellidoChequeado = true;
-
-function checkearDatos() {
-  while (nombre.length < 3 || nombreChequeado !== true) {
+function checkearDatos(nombre, apellido) {
+  while (nombre.length < 3 || !/^[A-Za-z]+$/.test(nombre)) {
     nombre = prompt("Por favor ingresa tu nombre");
-    nombreChequeado = /^[A-Za-z]+$/.test(nombre);
   }
-  while (apellido.length < 3 || apellidoChequeado !== true) {
+  while (apellido.length < 3 || !/^[A-Za-z]+$/.test(apellido)) {
     apellido = prompt("Por favor ingresa tu apellido");
-    apellidoChequeado = /^[A-Za-z]+$/.test(apellido);
   }
   if (edad >= 18) {
     alert("Bienvenido " + nombre + " " + apellido);
@@ -26,8 +18,11 @@ function checkearDatos() {
   }
 }
 
-checkearDatos();
+let nombre = prompt("Hola! ingresa tu nombre, por favor!");
+let apellido = prompt("Ahora tu apellido.");
+let edad = parseInt(prompt("Cual es tu edad? Gracias"));
 
+checkearDatos(nombre, apellido);
 alert(
   "Tener en cuenta que es un ciclo de 3 interacciones, pero si el resultado es 5 se corta el ciclo. (Ejemplo: Ingresando 5 - 2 - 2)"
 );
